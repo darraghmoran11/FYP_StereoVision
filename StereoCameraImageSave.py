0838195e7ccc8d34ce8364cd311e118ef98ba7f0
Darragh <darraghmoran11@hotmail.com>
2017-02-19 17:47:53 +0000

    Clean up the code for StereoCameraImageSave.py

--- StereoCameraImageSave.py
+++ StereoCameraImageSave.py
@@ -25,13 +25,14 @@ while(True):
     elif k%256 == 32:
         # SPACE pressed
         imgL_name = "imgL.png"
-        cv2.imwrite(imgL_name, frameLeft)
-        print("{} written!".format(imgL_name))
-
-   # elif k % 256 ==ord("q"):
         imgR_name = "imgR.png"
+
+        cv2.imwrite(imgL_name, frameLeft)
         cv2.imwrite(imgR_name, frameRight)
+
+        print("{} written!".format(imgL_name))
         print("{} written!".format(imgR_name))
+        print("************Images saved**********")
 
 capLeft.release()
 capRight.release()
